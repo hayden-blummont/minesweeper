f2599c3e2c305d8c0ae701371d6be83e194516fc
Hayden Blummont <hblummont@gmail.com>
2018-05-14 22:05:26 +1200

    add mine counting logic

--- minesweeper.js
+++ minesweeper.js
@@ -5,17 +5,14 @@ var board = {
   cells: []
 }
 
-
-
-
-
 var row, col;
 for (row = 0; row < 5; row++) {
   for (col = 0; col < 5; col++) {
     board.cells.push({
       row: row,
       col: col,
-      isMine: false,
+      isMine: Math.random() <= 0.2,
+      isMarked: false,
       hidden: true
     });
   }
@@ -23,8 +20,15 @@ for (row = 0; row < 5; row++) {
 
 
 
+
+
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  var i;
+  for (i = 0; i < board.cells.length; i++) {
+    var numMines = countSurroundingMines (board.cells[i]); 
+    board.cells[i].surroundingMines = numMines;
+  }
   lib.initBoard()
 }
 
@@ -48,5 +52,15 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var numMines = 0;
+  var surroundingCells = getSurroundingCells(cell.row, cell.col);
+  var i;
+  for (i = 0; i < surroundingCells.length; i++) {
+    if (surroundingCells[i].isMine) {
+      numMines++;
+    }
+  }
+
+  return numMines;
 }
 
